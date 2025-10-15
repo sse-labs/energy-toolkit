955cd0a1a81fce39146ec7d93039904605a0f6a6
Maximilian Krebs <Maximilian-krebs@online.de>
2025-10-16 01:10:24 +0200

    feat: Added actual program execution to the lib

--- energy_toolkit/program.py
+++ energy_toolkit/program.py
@@ -2,6 +2,10 @@
 Program abstraction
 """
 
+import subprocess
+
+from energy_toolkit.logger import Logger
+
 class Program:
     """
     Program abstraction class, that models a program that can be executed.
@@ -15,15 +19,38 @@ class Program:
         """
         Create a new Program object
         """
-        self.executeable = exe
-        self.arguments = args
-        self.inputfile = inpfile
+        self._executeable = exe
+
+        if args is None:
+            self._arguments = []
+        else:
+            self._arguments = args
+
+        self._inputfile = inpfile
 
     def execute(self, core=0):
         """
         Execute the program on a specific core
         """
-        print(core)
+        arguments = " ".join(self._arguments)
+        ex = self._executeable
+
+        if self._inputfile:
+            command = f"taskset -c {core} {ex} {arguments} < {self._inputfile} > /dev/null"
+        else:
+            command = f"taskset -c {core} {ex} {arguments} > /dev/null"
+
+        try:
+            subprocess.run(
+                command,
+                shell=True,           # Needed to use `<`
+                capture_output=True,   # Capture stdout and stderr
+                text=True,              # Get output as string
+                check=True
+            )
+
+        except Exception as e: # pylint: disable=broad-exception-caught
+            Logger().get_logger().error(e)
 
     def get_executeable(self):
         """Return the executeable attribute"""
