8f986044d37acfba200df5c075d1fe7eb0f9e039
Maximilian Krebs <maximilian.krebs@iml.fraunhofer.de>
2025-10-15 14:08:38 +0200

    feat: Added writing of results and writing of statistics. Statistics WIP

--- energy_toolkit/energy_toolkit.py
+++ energy_toolkit/energy_toolkit.py
@@ -2,7 +2,9 @@ import time
 from energy_toolkit.rapl_interface import RAPL_Interface
 from typing import Dict, List
 from energy_toolkit.program import Program
+from energy_toolkit.logger import Logger
 import numpy as np
+import os
 
 from energy_toolkit.util import Datapoint
 
@@ -16,11 +18,19 @@ class Energy_Toolkit():
 
   _programs: List[Program]  = []
 
-  def __init__(self, datapoints=100, repetitions=100, core = 0, programs = []):
+  _results: Dict[str, np.ndarray] = {}
+  _statistics: Dict[str, np.ndarray] = {}
+
+  _result_path = "./results"
+  _logger = None
+
+  def __init__(self, datapoints=100, repetitions=100, core = 0, programs = [], resultpath = "./results"):
     self._datapoints = datapoints
     self._repetitions = repetitions
     self._core = core
     self._programs = programs
+    self._result_path = resultpath
+    self._logger = Logger().get_logger()
 
   def add_program(self, program: Program) -> None:
     """
@@ -34,7 +44,7 @@ class Energy_Toolkit():
     """
     self._programs.clear()
 
-  def measure(self) -> Dict[str, np.ndarray]:
+  def measure(self) -> None:
     """
     Executes the programs added to the toolkit after another and measures the energy for each of the programs
     """
@@ -97,36 +107,129 @@ class Energy_Toolkit():
       for key, dplist in program_energy_usage.items()
     }
 
-    return arrays
+    self._results = arrays
+    self._generate_statistics()
 
-  def print_statistics(self, results: Dict[str, np.ndarray]) -> None:
-    """Prints some statistic metrics for the given results returned from a measurement"""
+
+  def _generate_statistics(self) -> None:
+    """Generate a statistics dict for further processing"""
+
+    statistics = {}
+
     # Iterate over program ids in the given results
-    for pid in results:
+    for pid in self._results:
+      program_stats = {}
+      time_dict = {}
+      energy_dict = {}
+
+      time_values = self._results[pid]['time']
+      energy_values = self._results[pid]['energy']
+
+      time_dict["mean"] = time_values.mean()
+      time_dict["variance"] = time_values.var()
+      time_dict["std_deviation"] = time_values.std()
+
+      energy_dict["mean"] = energy_values.mean()
+      energy_dict["variance"] = energy_values.var()
+      energy_dict["std_deviation"] = energy_values.std()
+
+      program_stats["time"] = time_dict
+      program_stats["energy"] = energy_dict
+
+      statistics[pid] = program_stats
+
+
+
+  def print_statistics(self) -> None:
+    """Prints some statistic metrics for the given results returned from a measurement"""
+    
+    # Iterate over program ids in the given results
+    for pid in self._statistics:
       # Retrieve the actual program with the program id
       program: Program = self._programs[pid]
 
-      time_values = results[pid]['time']
-      energy_values = results[pid]['energy']
+      time_values = self._statistics[pid]['time']
+      energy_values = self._statistics[pid]['energy']
 
       output = f"""
       ====================================
       Program {pid}: {program.executeable}
 
       Time:
-        AVG: {time_values.mean()} s
-        VAR: {time_values.var()} s
-        STD: {time_values.std()} s
+        AVG: {time_values["mean"]} s
+        VAR: {time_values["variance"]} s
+        STD: {time_values["std_deviation"]} s
 
       Energy:
-        AVG: {energy_values.mean()} J
-        VAR: {energy_values.var()} J
-        STD: {energy_values.std()} J
+        AVG: {energy_values["mean"]} J
+        VAR: {energy_values["variance"]} J
+        STD: {energy_values["std_deviation"]} J
       ====================================
       """
 
       print(output)
 
+  def get_results(self) -> Dict[str, np.ndarray]:
+    """Return the currently saved results"""
+    return self._results
+
+  def get_statistics(self) -> Dict[str, Dict[str, float]]:
+    """Return the currently saved statistics"""
+    return self._statistics
+  
+  def _create_location_if_not_exists(self, location) -> bool:
+    """Helper method that creates a folder at a location if it does not exists. If an error occurs an error message is printed"""
+    try:
+      if not os.path.exists(location):
+        os.makedirs(location)
+
+      return True
+    except:
+      self._logger.error("Creation of result location failed!")
+      return False
+    
+  
+  def write_results(self):
+    """Write the last saved results to a file. One file for each program under analysis at the specific result location"""
+    folder_successfully_created = self._create_location_if_not_exists(self._result_path)
+
+    if folder_successfully_created:
+      for pid in self._results:
+        data = np.column_stack((self._results[pid]['time'], self._results[pid]['energy']))
+
+        savefolder = os.path.join(self._result_path, str(pid))
+        pid_folder_created = self._create_location_if_not_exists(savefolder)
+
+        if pid_folder_created:
+          savelocation = os.path.join(savefolder, "results.csv")
+          np.savetxt(savelocation, data, header='Time,Energy', delimiter=',', fmt='%.6f')
+        else:
+          self._logger.error("File could not be saved! Do you habe the correct rights to access the result location?")
+
+    else:
+      self._logger.error("File could not be saved! Do you habe the correct rights to access the result location?")
+
+
+  def write_statistics(self):
+    """Write the last saved statistics to a file. One file for each program under analysis at the specific result location"""
+    folder_successfully_created = self._create_location_if_not_exists(self._result_path)
+
+    if folder_successfully_created:
+      for pid in self._results:
+        print(self._statistics)
+        data = np.column_stack((self._statistics[pid]['time'], self._statistics[pid]['energy']))
+
+        savefolder = os.path.join(self._result_path, str(pid))
+        pid_folder_created = self._create_location_if_not_exists(savefolder)
+
+        if pid_folder_created:
+          savelocation = os.path.join(savefolder, "statistics.csv")
+          np.savetxt(savelocation, data, header='Time,Energy', delimiter=',', fmt='%.6f')
+        else:
+          self._logger.error("File could not be saved! Do you habe the correct rights to access the result location?")
+
+    else:
+      self._logger.error("File could not be saved! Do you habe the correct rights to access the result location?")
 
     
 
