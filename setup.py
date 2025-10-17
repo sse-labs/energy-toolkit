c19e55368137447901d0185e3b2620abab56ea40
Maximilian Krebs <Maximilian-krebs@online.de>
2025-10-17 15:29:37 +0200

    feat: Changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="energy_toolkit",
-    version="0.1",
+    version="1.0",
     packages=find_packages(),
     description="Provides functionality to benchmark a program and measure time and energy during execution.",
     long_description=open("README.md").read(),
